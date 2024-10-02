ed657cef32e6f74168cac4e7249e4e237723a32c
ulisandro2 <sanchezulises952@gmail.com>
2024-10-02 00:30:42 -0300

    70% el nuevo malala

---
+++ js/index.js
@@ -0,0 +1,77 @@
+// Script para el scroll 
+document.addEventListener('DOMContentLoaded', function() {
+    let lastScrollTop = 0;
+    const menu = document.querySelector('.menu ul');
+
+    window.addEventListener('scroll', function() {
+        let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+        if (scrollTop > lastScrollTop) {
+            // Scroll hacia abajo: oculta el menú
+            menu.style.opacity = '0';
+            menu.style.transition = 'opacity 0.2s ease-out';
+        } else {
+            // Scroll hacia arriba o detención del scroll: muestra el menú
+            menu.style.opacity = '1';
+            menu.style.transition = 'opacity 0.2s ease-in';
+        }
+        
+        lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // Asegura que lastScrollTop no sea negativo
+    });
+});
+
+
+
+// Galeria de imagenes
+
+document.addEventListener('DOMContentLoaded', () => {
+    const imgGallery = document.querySelector('.img-gallery');
+    const images = imgGallery.children;
+
+    function swapImages(index1, index2) {
+        const temp = images[index1].src;
+        images[index1].src = images[index2].src;
+        images[index2].src = temp;
+    }
+
+    // Secuencia personalizada de cambios
+    function startImageRotation() {
+        setTimeout(() => {
+            swapImages(0, 2); // Cambiar la primera imagen con la tercera
+        }, 4000);
+
+        setTimeout(() => {
+            swapImages(1, 7); // Cambiar la segunda imagen con la última
+        }, 5000);
+
+        setTimeout(() => {
+            swapImages(3, 5); // Cambiar la cuarta imagen con la sexta
+        }, 8000);
+
+        setTimeout(() => {
+            swapImages(6, 4); // Cambiar la séptima imagen con la quinta
+        }, 4000);
+    }
+
+    // Repetir la secuencia cada 15 segundos
+    setInterval(startImageRotation, 12000);
+
+    // Iniciar la primera rotación
+    startImageRotation();
+});
+
+
+// Galaria de imagenes ligthbox
+
+const fullImgBox = document.getElementById('fullImgBox');
+const fullImg = document.getElementById('fullImg');
+
+const openFullImg = (pic) => {
+    fullImgBox.style.display= "flex";
+    fullImg.src = pic;
+}
+
+const closeFullImg = () => {
+    fullImgBox.style.display = "none"
+}
+
